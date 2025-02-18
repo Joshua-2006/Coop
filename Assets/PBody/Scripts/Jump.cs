4e8879cd441520b9265bd52d4bbd4d722b41c6ee
ThyCoolPerson <jradchishin@gmail.com>
2025-02-18 09:37:52 -0800

    Jump

---
+++ Assets/PBody/Scripts/Jump.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
